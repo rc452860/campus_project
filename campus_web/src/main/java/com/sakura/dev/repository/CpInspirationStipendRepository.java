8bb4f249169a36e6d593f2ec1af75f537c59fea7
sun_123 <731266793@qq.com>
2017-06-05 11:30:33 +0800

    添加Repository类

---
+++ campus_web/src/main/java/com/sakura/dev/repository/CpInspirationStipendRepository.java
@@ -0,0 +1,10 @@
+package com.sakura.dev.repository;
+
+import com.sakura.dev.domain.CpInspirationalStipend;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by yth on 2017/6/5.
+ */
+public interface CpInspirationStipendRepository extends JpaRepository<CpInspirationalStipend,String> {
+}
---
+++ campus_web/src/main/java/com/sakura/dev/repository/CpStipendApplicationRepository.java
@@ -0,0 +1,10 @@
+package com.sakura.dev.repository;
+
+import com.sakura.dev.domain.CpStipendApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by yth on 2017/6/5.
+ */
+public interface CpStipendApplicationRepository extends JpaRepository<CpStipendApplication,String> {
+}
